55a27d441ef0a9c89136122c8e08f2c9ec87200a
chiawench <chiawen.ch@gmail.com>
2017-09-21 20:13:36 +0800

    not finished

--- Python/canny edge detector/canny_edge_detector.py
+++ Python/canny edge detector/canny_edge_detector.py
@@ -29,22 +29,24 @@ def arc(im):
 
 
 
-im = array(Image.open('test.jpg').convert('L'))
+im = array(Image.open('test.png').convert('L'))
 sigma = 2
 
 
 imgx=imx(im, sigma)
-imshow(imgx)
+#imshow(imgx)
 imgy=imy(im, sigma)
-imshow(imgy)
+#imshow(imgy)
 imgmag=mag(im, sigma)
-imshow(imgmag)
+#imshow(imgmag)
 arc = arc(im)
 (x,y) = arc.shape
 print(x)
 print(y)
 
-print(arc[:,1])
+ime = Image.fromarray(uint8(imgmag))
+ime.save("mag.jpg")
+nms = zeros(im.shape)
 
 for i in range(x):
     for j in range(y):
@@ -68,7 +70,35 @@ for i in range(x):
                  arc[i,j] = 180
 
 
+for i in range(1,x-1):
+    for j in range(1,y-1):
+        if(arc[i,j] == 0 or arc[i,j] == 180) :
+            if(imgmag[i,j] >imgmag[i,j-1] and imgmag[i,j] > imgmag[i,j+1]) :
+                nms[i,j] = 128
+            else:
+                nms[i, j] = 0
+        if (arc[i, j] == 45 or arc[i, j] == -135):
+            if (imgmag[i, j] > imgmag[i-1, j + 1] and imgmag[i, j] > imgmag[i+1, j - 1]):
+                nms[i, j] = 128
+            else:
+                nms[i, j] = 0
+        if (arc[i, j] == 90 or arc[i, j] == -90):
+            if (imgmag[i, j] > imgmag[i-1, j ] and imgmag[i, j] > imgmag[i+1, j]):
+                nms[i, j] = 128
+            else:
+                nms[i, j] = 0
+        if (arc[i, j] == 135 or arc[i, j] == -45):
+            if (imgmag[i, j] > imgmag[i-1, j - 1] and imgmag[i, j] > imgmag[i+1, j + 1]):
+                nms[i, j] = 128
+            else:
+                nms[i, j] = 0
 
-print(arc[:,1])
+ime = Image.fromarray(uint8(nms))
+ime.save("nms.jpg")
+
+
+
+
+
 
 
